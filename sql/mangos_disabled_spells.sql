f59c44f4f4ddea1fc3f90c81d747c93192ab7d8a
kero99 <motrilwireless@gmail.com>
2009-11-14 18:49:04 +0100

    Crash Spells Disabling

---
+++ sql/mangos_disabled_spells.sql
@@ -0,0 +1,8 @@
+insert into `spell_disabled` (`entry`, `ischeat_spell`, `active`) values('37565','0','1');
+insert into `spell_disabled` (`entry`, `ischeat_spell`, `active`) values('54181','0','1');
+insert into `spell_disabled` (`entry`, `ischeat_spell`, `active`) values('53168','0','1');
+insert into `spell_disabled` (`entry`, `ischeat_spell`, `active`) values('67358','0','1');
+insert into `spell_disabled` (`entry`, `ischeat_spell`, `active`) values('33897','0','1');
+insert into `spell_disabled` (`entry`, `ischeat_spell`, `active`) values('47762','0','1');
+insert into `spell_disabled` (`entry`, `ischeat_spell`, `active`) values('32216','0','1');
+insert into `spell_disabled` (`entry`, `ischeat_spell`, `active`) values('55637','0','1');
