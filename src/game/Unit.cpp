c463dc3a44e7348706e7683bce3a6606db8b54cf
kero99 <motrilwireless@gmail.com>
2009-11-15 20:03:05 +0100

    [K15] Crash Fix, Improvement Unit::AddThreat Function

--- src/game/Unit.cpp
+++ src/game/Unit.cpp
@@ -11015,6 +11015,8 @@ float Unit::ApplyTotalThreatModifier(float threat, SpellSchoolMask schoolMask)
 
 void Unit::AddThreat(Unit* pVictim, float threat /*= 0.0f*/, bool crit /*= false*/, SpellSchoolMask schoolMask /*= SPELL_SCHOOL_MASK_NONE*/, SpellEntry const *threatSpell /*= NULL*/)
 {
+	if(!pVictim)
+         return;
     // Only mobs can manage threat lists
     if(CanHaveThreatList())
     {
